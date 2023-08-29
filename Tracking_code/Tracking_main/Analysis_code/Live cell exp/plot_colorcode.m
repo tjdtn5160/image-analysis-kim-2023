c818b05a6bc9cfcaba57b38fb46338d7ec531034
tjdtn5160 <55391699+tjdtn5160@users.noreply.github.com>
2023-08-29 18:13:43 -0400

    Delete Tracking_code/Tracking_main directory

--- Tracking_code/Tracking_main/Analysis_code/Live cell exp/plot_colorcode.m
+++
@@ -1,6 +0,0 @@
-function h = plot_colorcode(x , y , c , linewidth)
-% Plot color-coded lines
-z = zeros(size(x));
-
-h = surface([x ; x] , [y ; y] , [z ; z] , [c ; c] , 'facecol' , 'no' , 'edgecol' , 'interp' , 'linew' , linewidth);
-end
